62fa24473b3b074713408529ec88732394505c3a
maespey <accounts@maespey.com>
2018-09-12 09:05:17 -0500

    Initial commit

---
+++ merge_users.py
@@ -0,0 +1,21 @@
+from canvasapi import Canvas
+import config
+
+
+canvas = Canvas(API_URL, API_KEY)
+
+def getLocalUser():
+    localUser = canvas.get_user(input("Enter the Local UserID from Canvas:"))
+    print("Here is the user you selected: " + str(localUser))
+    return localUser
+
+def getHawkIDUser():
+    guestHawkID = canvas.get_user(input("Enter the new Guest HawkID's UserID in Canvas:"))
+    print("Here is the user you selected: " + str(guestHawkID))
+    return guestHawkID
+
+
+
+newUser = localUser.merge_into(guestHawkID)
+print("You have successfully merged the accounts. The merged user is: " = str(newUser))
+
