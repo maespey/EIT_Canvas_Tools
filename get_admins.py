595154bd2bb05c7a435ef411a4248e502f6acf9a
maespey <accounts@maespey.com>
2018-09-18 16:47:30 -0500

    Adding content for DOE org changes

---
+++ get_admins.py
@@ -0,0 +1,50 @@
+from canvasapi import Canvas
+import config
+import csv
+
+canvas = Canvas(config.API_URL, config.API_KEY)
+
+accountIDs = [6,7,8,9,10,11,12,13,14,15,16,17,18]
+
+filename = "DOE_Admin.csv"
+
+writer = None
+
+csv = open(filename, "w")
+csv.write("username,role,role_id,subaccount\n")
+
+def build_admin_lst(account, mode):
+
+    print("\n_______________________\n" + "Admin's for: " + str(account))
+    root_admins = account.get_admins()
+    print("*****Root Admins*****\n")
+
+    for admin in root_admins: # Grabs all root level Admin Users
+        user = admin.user
+        role = admin.role
+        print("Name: " + user['login_id'] + " -> Role: " + role)
+
+        csv.write(str(user['login_id'])+","+role+","+str(admin.id)+","+str(account)+"\n")
+
+
+    if mode == "all":
+        subaccounts = account.get_subaccounts(True) # Get all subaccounts in an account
+        for subaccount in subaccounts:
+            #print("     " + str(subaccount))
+            admins = subaccount.get_admins() # Get all Admin Users
+            tmp = []
+            for admin in admins:
+                user = admin.user
+                role = admin.role
+                # print("      Name: " + user['name'] + " -> Role: " + role)
+
+    else:
+        return None
+
+
+for ID in accountIDs:
+    current_account = canvas.get_account(ID)
+
+    build_admin_lst(current_account, "root") # Account, Mode("root" or "all")
+
+print("Your .CSV is done...")
--- merge_users.py
+++ merge_users.py
@@ -1,20 +1,72 @@
 from canvasapi import Canvas
 import config
+import csv
 
 
 canvas = Canvas(config.API_URL, config.API_KEY)
 
-def getLocalUser():
-    localUser = canvas.get_user(input("Enter the Local UserID from Canvas:"))
-    print("Here is the user you selected: " + str(localUser))
+def getLocalUser(ID):
+    if ID == None:
+        localUser = canvas.get_user(\
+            input("Enter the Local UserID from Canvas:"))
+        print("Here is the user you selected: " + str(localUser))
+
+    else:
+        localUser = canvas.get_user(ID)
     return localUser
 
-def getHawkIDUser():
-    guestHawkID = canvas.get_user(input("Enter the new Guest HawkID's UserID in Canvas:"))
-    print("Here is the user you selected: " + str(guestHawkID))
-    return guestHawkID
+def getHawkIDUser(ID):
+    if ID == None:
+        guestUser = canvas.get_user(\
+            input("Enter the new Guest HawkID's UserID in Canvas:"))
+        print("Here is the user you selected: " + str(guestUser))
+
+    else:
+        guestUser = canvas.get_user(ID)
+    return guestUser
 
 
-newUser = localUser.merge_into(guestHawkID)
-print("You have successfully merged the accounts. The merged user is: " = str(newUser))
+##___________________________________________________________________________##
+
+if (input("Would like to read from users.csv? (Y/N)  ")\
+            in ["Y", "y", "yes", "Yes"]):
+    with open('users.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        merge_count = 0
+        for row in csv_reader:
+            if merge_count == 0:
+                print("\n\nMerging users from users.csv...")
+                merge_count += 1
+
+            else:
+                localUser = getLocalUser(row[0])
+                guestUser = getHawkIDUser(row[1])
+                print(f'  -> merging {localUser} into {guestUser}')
+                localUser.merge_into(guestUser)
+                merge_count += 1
+
+        print(f'{merge_count-1} accounts have been merged on {config.domain}.')
+
+
+else:
+    more_users = True
+    merge_count = 0
+    while more_users:
+        # localUser = getLocalUser()
+        # guestUser = getHawkIDUser()
+
+        newUser = "guestUser"
+
+        #newUser = localUser.merge_into(guestUser)
+        print("You have successfully merged the accounts."\
+            "\n  -> The merged user is: " + str(newUser))
+
+        if (input("Do you have more to merge? (Y/N)  ")\
+                in ["Y", "y", "yes", "Yes"]):
+            more_users = True
+        else: more_users = False
+        merge_count += 1
+        print("")
+
+    print(f"{merge_count} users have been merged\n\n")
 
