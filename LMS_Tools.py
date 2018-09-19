fa409f5284c598205292e755eefba6d1beccdef0
maespey <accounts@maespey.com>
2018-09-19 14:50:51 -0500

    cleanup

--- LMS_Tools.py
+++ LMS_Tools.py
@@ -3,19 +3,5 @@ import config
 
 canvas = Canvas(API_URL, API_KEY)
 
-# user = canvas.get_user(161540)
-# print(user)
-
-# channels = user.get_communication_channels()
-
-# for channel in channels:
-#   print(channel)
-
-def getLocalUser():
-  localUser = canvas.get_user(input("Enter the Local UserID from Canvas:"))
-  print("Here is the user you selected:  " + str(localUser))
-
-def getHawkIDUser():
-  guestHawkID = canvas.get_user(input("Enter the new Guest HawkID's UserID in Canvas:"))
-  print("")
-
+user = canvas.get_user(161540)
+print(user)
