c93ca67b8bfc5e4811d8a702310e5eb79a396a03
Michael Espey <heroku@maespey.com>
2022-12-14 02:01:31 -0600

    Get Modules - Working

--- search_course.py
+++ search_course.py
@@ -4,8 +4,36 @@ import json
 
 headers = {'Authorization' : 'Bearer ' + '%s' % config.API_KEY}
 
-pages_checked = {}
-module_items_checked = {}
+pages = {}
+module_items = {} # { (Module Item ID) : [ (Item Type), (Has the item been checked) ] }
+discussions = {}
+assignments = {}
+quiz = {}
+announcements = {}
+
+##### PAGES #####
+def checkPage(module_id):
+    None
+
+##### DISCUSSIONS #####
+def checkDisc(module_id):
+    None
+
+##### ASSIGNMENTS #####
+def checkAssignments(module_id):
+    None
+
+##### QUIZZES #####
+def checkQuiz(module_id):
+    None
+
+##### ANNOUNCEMENTS #####
+def checkAnnouncement(module_id):
+    None
+
+##### MODULES #####
+def checkExternalURL(module_id):
+    None
 
 def getModules(course_id):
     payload = {'include': 'items'}
@@ -13,8 +41,14 @@ def getModules(course_id):
     results = requests.get(url, params=payload, headers = headers)
 
     response = results.text
-    print(response)
 
+    course = json.loads(response)
+    for module in course:        
+        for item in module['items']:
+            if item['type'] == 'ExternalTool':
+                module_items[item['id']] = [item['type'], False]
+            
+##### CONFIRM COURSE #####
 def checkCourse(course_id):
     url = config.API_URL + 'api/v1/courses/' + course_id
     results = requests.get(url, headers = headers)
@@ -23,28 +57,29 @@ def checkCourse(course_id):
         print("    There are no courses with that ID...")
     else:
         response = results.text
-
         course = json.loads(response)
-        return(course['name'])
+        
+        print(course['name'])
+        if input("...is that the correct user? (Y/N)  ")\
+            in ["Y", "y", "yes", "Yes", "YES", "YAS"]:
+            return(course['name'])
+        else:
+            print("Cancel...")
+            return False
+
+        
 
 def main():
     course_id = input("Enter the Course ID in Canvas: ")
 
     course_name = checkCourse(course_id)
-    print("\n  Course Name: "+ course_name)
+    if course_name == False:
+        return
+    print("\nCourse Name: " + course_name)
 
     getModules(course_id)
 
-    # Building the request
-    # payload = {'search_term': user_id}
-    # url = config.API_URL + 'api/v1/courses/' + course_id
-    # print(url)
-    # results = requests.get(url, headers = headers)
-    # print(results.status_code)
-
-    # Sending/Recieving the response
-    # results = requests.get(url, headers = headers)
-
+    print(module_items)
 
 if __name__ == "__main__":
     main()
